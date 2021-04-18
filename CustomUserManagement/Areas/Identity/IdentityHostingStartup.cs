f17797ef768275df46a1d703d90570438ed9bace
Emre Toraman <emretoraman@gmail.com>
2021-04-18 23:34:38 +0300

    scaffold the identity ui

---
+++ CustomUserManagement/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using CustomUserManagement.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(CustomUserManagement.Areas.Identity.IdentityHostingStartup))]
+namespace CustomUserManagement.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
